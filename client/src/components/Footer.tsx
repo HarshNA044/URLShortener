1b88fec9895d9339a465528933425aa605ef834c
Harsh <152859342+HarshNA044@users.noreply.github.com>
2024-06-26 11:15:55 +0530

    Create Footer.tsx

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+// components/Footer.tsx
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer style={{ padding: "1rem", textAlign: "center", backgroundColor: "#f1f1f1" }}>
+      <p>&copy; 2024 ShortURL. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
